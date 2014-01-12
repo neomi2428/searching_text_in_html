42f61eb257fe61790c6d4f930b651825202178d2
jace <jace@JunheePark>
2014-01-11 22:51:14 -0500

    testing Jsoup

--- src/searchingtextinhtml/SearchingTextInHTML.java
+++ src/searchingtextinhtml/SearchingTextInHTML.java
@@ -1,19 +1,35 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package searchingtextinhtml;
 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 /**
- *
- * @author jace
+ * The public access point to search text in html.
+ * This uses Jsoup library.
+ * 
+ * @author Junhee Park
  */
 public class SearchingTextInHTML {
 
 	/**
 	 * @param args the command line arguments
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Hello, World!");
+		testJsoup();
+	}
+
+	/**
+	 * This is for testing Jsoup.
+	 * 
+	 * @throws IOException 
+	 */
+	public static void testJsoup() throws IOException {
+		Document doc = Jsoup.connect("http://career.stonybrook.edu").get();
+		Elements body = doc.select("body");
+		
+		System.out.println(body);
 	}
 }
